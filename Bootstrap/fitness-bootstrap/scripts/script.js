cd305faf5d6b32fc9b2a9c352a33b68e1976aa36
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-04-22 22:38:00 +0200

    finished working with bootstrap on forward fitness on 22.04.2022

---
+++ Bootstrap/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Your name
+    File Name: script.js 
+    Date: Today’s date
+*/
+// jquery for hero image to consume the header window space
+$(document).ready(function () {
+    $(".hero").height($(window).height())
+});
